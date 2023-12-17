d3e84c27cfc797496c8d6a4cdacb46e834dc573c
Juan Gallardo <juangallardovergara17@gmail.com>
2023-12-17 20:43:27 -0300

    feat: agregadas redes sociales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+'use client';
 import Image from 'next/image'
 import styles from './home.module.css'
+import Link from 'next/link';
+
+const links = [
+  {
+    name: 'Comissions Prices',
+    href: '/'
+  },
+  {
+    name: 'Terms of Service',
+    href: '/'
+  },
+  {
+    name: 'About Me',
+    href: '/'
+  },
+  {
+    name: 'Gallery',
+    href: '/'
+  }
+]
+
+const copyToClipboard = () => {
+  navigator.clipboard
+      .writeText("Shadboi#")
+      .then(() => {
+        alert("successfully copied");
+      })
+      .catch(() => {
+        alert("something went wrong");
+      });
+}
 
 export default function Home() {
   return (
@@ -18,15 +50,53 @@ export default function Home() {
         <h1 className={styles.title}>Shadboi</h1>
         <h3 className={styles.subtext}>Comissions<br/><span>Open</span></h3>
         <div className={styles.contbtn}>
-          <button className={styles.btn}>Comissions Prices</button>
-          <button className={styles.btn}>Terms of Service</button>
-          <button className={styles.btn}>About Me</button>
-          <button className={styles.btn}>Gallery</button>
+        {links.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={styles.btn}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
         </div>
       </div>
-      {/* <div className={styles.socials}>
-
-      </div> */}
+      <div className={styles.socials}>
+          <a href="#">
+            <Image
+              src="/logo_newgrounds.svg"
+              alt="Newgrounds"
+              width={82}
+              height={80}
+            />
+          </a>
+          <button onClick={copyToClipboard}>
+            <Image
+              src="/logo_discord.svg"
+              alt="Discord"
+              width={82}
+              height={80}
+            />
+          </button>
+          <a href="#">
+            <Image
+              src="/logo_x.svg"
+              alt="X"
+              width={82}
+              height={80}
+            />
+          </a>
+          <a href="#">
+            <Image
+              src="/logo_steam.svg"
+              alt="Steam"
+              width={82}
+              height={80}
+            />
+          </a>
+      </div>
     </main>
   )
 }
